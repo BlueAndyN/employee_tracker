a75d9b12a0a8722c2d8e31b9fc9247f6af4cdbce
Andrew Nguyen <blueandyn@gmail.com>
2022-08-20 20:12:45 -0600

    renewed information and simplified seeds

--- db/Schema.sql
+++ db/Schema.sql
@@ -1,29 +1,28 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
-USE employee_db; 
+DROP DATABASE IF EXISTS employeesDB;
+
+CREATE DATABASE employeesDB;
+
+USE employeesDB;
 
 CREATE TABLE department (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE roles (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL, 
-    salary DECIMAL NOT NULL,
-    department_id INTEGER, 
-    INDEX dep_ind (department_id),
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10.3) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    roles_id INTEGER, 
-    INDEX roles_ind (roles_id),
-    CONSTRAINT fk_role FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE SET NULL,
-    manager_id INTEGER,
-    INDEX manager_ind (manager_id),
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,15 +1,20 @@
-INSERT INTO department(name)
-VALUES("Sales"), ("Engineering"), ("Finance"), ("Legal");
+USE employeesDB;
+
+INSERT INTO department (name)
+VALUES ("Sales");,
+VALUES ("Engineering");.
+VALUES ("Finance");,
+VALUES ("Legal");
 
 INSERT INTO roles (title, salary, department_id)
-VALUES("Finacial Manager", 120000, 1), ("Finacial Rep", 65000, 1), 
-("Lead Engineer", 140000, 2), ("Software Engineer", 85000, 2), 
-("Inventory Manager", 130000, 3), ("Department Manager", 80000, 3), 
-("Counsel", 160000, 4), ("Lawyer", 95000, 4);
+VALUES ("Sales Lead", 100000, 1);,
+VALUES ("Lead Engineer", 150000, 2);,
+VALUES ("Software Engineer", 120000, 6287);,
+VALUES ("Accountant", 125000, 3);,
+VALUES ("Legal Team Lead", 250000, 4);,
 
-INSERT INTO employee (first_name, last_name, roles_id, manager_id)
-VALUES ("David", "luna", 2, null),
-("Belveth", "Nickels", 1, 1),
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Belveth", "Nickels", 1, 1),
 ("Kevin", "Hart", 4, null),
 ("Diona", "eula", 3, 3),
 ("Sarah", "Kerrigan", 6, null),
